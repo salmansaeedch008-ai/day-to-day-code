4de0536c637cff2a3008cdfce34814ddbe69be3e
salmansaeedch008-ai <salmansaeedch008@gmail.com>
2025-11-27 22:51:47 +0500

    Basics of file handling

---
+++ file_handling.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    fstream myfile;
+    myfile.open("name.txt", ios::in);
+
+    if(!myfile){
+        cout << "File not found!";
+        return 0;
+    }
+
+    string x;
+    myfile >> x;
+    cout << x;
+
+    myfile.close();
+    return 0;
+}
+
